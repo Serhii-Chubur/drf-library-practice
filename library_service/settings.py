503665895695e3f5ff1f39a0b6b7a75d15c64866
Serhii-Chubur <serhichubur15@gmail.com>
2025-02-10 19:16:39 +0200

    create User serializer

--- library_service/settings.py
+++ library_service/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
+    "rest_framework",
     "user",
 ]
 
@@ -124,3 +126,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
