9aa0a3fd7f3abeca497a445b4ab21c2e528641b8
Serhii-Chubur <serhichubur15@gmail.com>
2025-02-14 16:27:52 +0200

    redo date validation

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -18,14 +18,23 @@ class BorrowingSerializer(serializers.ModelSerializer):
         read_only_fields = ("id", "user", "actual_return_date")
 
     def create(self, validated_data):
+        print(validated_data)
         validated_data["user"] = self.context["request"].user
         validated_data["actual_return_date"] = None
         validated_data["borrow_date"] = datetime.date.today()
         book = validated_data["book"]
         if book.inventory == 0:
             raise serializers.ValidationError("Book is not available")
+        if (
+            validated_data["expected_return_date"]
+            <= validated_data["borrow_date"]
+        ):
+            raise serializers.ValidationError(
+                "Expected return date must be after borrow date."
+            )
         book.inventory -= 1
         book.save()
+
         return super().create(validated_data)
 
 
@@ -48,6 +57,17 @@ class BorrowingReturnSerializer(serializers.ModelSerializer):
             "book",
             "user",
         )
+
+    def update(self, instance, validated_data):
+        print(validated_data)
+        if (
+            validated_data["actual_return_date"]
+            and validated_data["actual_return_date"] < instance.borrow_date
+        ):
+            raise serializers.ValidationError(
+                "Actual return date must be on or after borrow date."
+            )
+        return super().update(instance, validated_data)
 
 
 class BorrowingListSerializer(serializers.ModelSerializer):
